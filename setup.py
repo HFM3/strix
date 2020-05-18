85bad465e386063b897d50b090a3be745d77f6cc
HFM3 <24279380+HFM3@users.noreply.github.com>
2020-05-18 01:54:03 -0400

    0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="strix",
-    version="0.7.0",
+    version="0.7.1",
     author="HFM3",
     # author_email="author@example.com",
     description="A Python Package for field-based GIS work",
